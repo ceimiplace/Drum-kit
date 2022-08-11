9665cf0749a5013fbebdfaabe70dae402355737e
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-08-11 20:57:00 +0300

    added click event listener
    added event listeners for every botton

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 let keys = document.querySelectorAll(".key");
+
 window.addEventListener("keydown", (e) => {
   console.log(e.code);
   let audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  let div = document.querySelector(`div[data-key="${e.code}"]`);
 
   console.log(audio);
   if (!audio) {
     return;
   }
+  audio.currentTime = 0;
   audio.play();
+  div.classList.add("active");
+});
+keys.forEach((elem) => {
+  elem.addEventListener("transitionend", () => {
+    elem.classList.remove("active");
+  });
+});
+//making for click-event;
+keys.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    let attribute = elem.getAttribute("data-key");
+    let audio = document.querySelector(`audio[data-key="${attribute}"]`);
+    if (!audio) {
+      return;
+    }
+    audio.currentTime = 0;
+    audio.play();
+    elem.classList.add("active");
+  });
 });
