73ef4f729dbb8210dcc1262e4fc8095e85ec3b93
ceimiplace <azamfiredenis@yahoo.com>
2022-08-11 19:33:55 +0300

    changed keyCode to .code,added event listener to play to keydown
    changed keyCode to .code,added event listener to play to keydown

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let keys = document.querySelectorAll(".key");
 window.addEventListener("keydown", (e) => {
-  console.log(e);
+  console.log(e.code);
+  let audio = document.querySelector(`audio[data-key="${e.code}"]`);
+
+  console.log(audio);
+  if (!audio) {
+    return;
+  }
+  audio.play();
 });
